f86c6dce88f4f3a71fd32177e05978e84dffeceb
Sebastian <sehernandezce@unal.edu.co>
2023-04-16 14:58:30 -0500

    version mejorada del v2

--- src/screen/capture.py
+++ src/screen/capture.py
@@ -1,7 +1,8 @@
-import mss
+from time import sleep
+import numpy as np
 import pyautogui
 import cv2
-import numpy as np
+import mss
 
 sct = mss.mss()
 
@@ -95,6 +96,101 @@ class capture:
         except:
             print("Error encontrando el azul")
             return -1,-1
+    
+    def scanYellow(self):
+        I = -1
+        J = -1
+        try:
+            if True:
+                img =  np.array(sct.grab(monitor=self.dim_board))
+                hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+                lower_Yellow = np.array([20, 50, 50])
+                upper_Yellow = np.array([30, 255, 255])
+
+                mask = cv2.inRange(hsv, lower_Yellow, upper_Yellow)
+                mask = cv2.medianBlur(mask, 5)
+                contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+                if len(contours) > 0:
+                    return True
+                    contour_sizes = [(cv2.contourArea(contour), contour) for contour in contours]
+                    biggest_contour = max(contour_sizes, key=lambda x: x[0])[1]
+                    M = cv2.moments(biggest_contour)
+                    if M["m00"] == 0:
+                        M["m00"] = 1
+                    centroid_x = int(M["m10"] / M["m00"])
+                    centroid_y = int(M["m01"] / M["m00"])
+
+                    y = centroid_y
+                    x = centroid_x
+                    I = np.floor(y/self.wi) + 1
+                    J = np.floor(x/self.hi) + 1
+                    # self.printScreen2(img, mask, "yelow", I, J, x, y)
+                    return int(I),int(J)
+            return False
+        except:
+            print("Error encontrando el azul")
+            return -1,-1
+
+    def scanBlueV2(self):
+        I = -1
+        J = -1
+        try:
+            while True:
+                img =  np.array(sct.grab(monitor=self.dim_board))
+                hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+                lower_blue = np.array([112, 172, 244])
+                upper_blue = np.array([140, 255, 255])
+
+                mask = cv2.inRange(hsv, lower_blue, upper_blue)
+                mask = cv2.medianBlur(mask, 5)
+                contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+                if len(contours) > 0:
+                    contour_sizes = [(cv2.contourArea(contour), contour) for contour in contours]
+                    biggest_contour = max(contour_sizes, key=lambda x: x[0])[1]
+                    M = cv2.moments(biggest_contour)
+                    if M["m00"] == 0:
+                        M["m00"] = 1
+                    centroid_x = int(M["m10"] / M["m00"])
+                    centroid_y = int(M["m01"] / M["m00"])
+
+                    y = centroid_y
+                    x = centroid_x
+                    I = np.floor(y/self.wi) + 1
+                    J = np.floor(x/self.hi) + 1
+                    # self.printScreen2(img, mask, "blue", I, J, x, y)
+                    return int(I),int(J)
+        except:
+            print("Error encontrando el azul")
+            return -1,-1
+    
+    def scanRedV2(self):
+        I = -1
+        J = -1
+        while True:
+            sleep(0.01)
+            img =  np.array(sct.grab(monitor=self.dim_board))
+            hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+            lower_red = np.array([0, 100, 100])
+            upper_red = np.array([10, 255, 255])
+
+            mask = cv2.inRange(hsv, lower_red, upper_red)
+            mask = cv2.medianBlur(mask, 5)
+            contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+            if len(contours) > 0:
+                contour_sizes = [(cv2.contourArea(contour), contour) for contour in contours]
+                biggest_contour = max(contour_sizes, key=lambda x: x[0])[1]
+                M = cv2.moments(biggest_contour)
+                if M["m00"] == 0:
+                    M["m00"] = 1
+                centroid_x = int(M["m10"] / M["m00"])
+                centroid_y = int(M["m01"] / M["m00"])
+
+                y = centroid_y
+                x = centroid_x
+                I = np.floor(y/self.wi) + 1
+                J = np.floor(x/self.hi) + 1
+                break
+        return int(I),int(J)
 
     def mousePosition(self):
         cnt = 0
@@ -117,3 +213,9 @@ class capture:
         cv2.imshow("Computer Vision", small)
         
 
+    def printScreen2(self, img, mask, color, I, J, x, y):
+        cv2.circle(img, (x,y), 3, (255,0,255),-1)
+        cv2.putText(img, f"({I}, {J})", (int(x), int(y)), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
+        cv2.imshow(f"Imagen con color {color}", img)
+        # cv2.imshow(f"mask {color}", mask)
+        cv2.waitKey(1)
